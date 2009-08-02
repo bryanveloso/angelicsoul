5db54f21bfdf66c32c2f9a55a1a20aa0437697e5
Bryan Veloso <bryan@revyver.com>
2009-08-01 22:34:12 -0700

    Version 1.0 of AngelicSoul. Will be upgrading the code to catch it up with any changes I make with ChaoticSoul.

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<div class="hr">&nbsp;</div>
+<div id="footer">
+    <p>Get a free blog at <a href="http://wordpress.com/">WordPress.com</a> and more themes at <a href="http://labs.revyver.com">Revyver Labs</a>. Theme: AngelicSoul (v1.0) by <a href="http://avalonstar.com" rel="designer">Bryan Veloso</a>.</p>
+</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
